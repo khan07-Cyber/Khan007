4d1d6ce813106c6d078338edbf2ead4ca1c1ef28
khan07-Cyber <kh4nkhan41@gmail.com>
2025-07-24 02:08:17 +0500

    Update KHAN.py

--- KHAN.py
+++ KHAN.py
@@ -1,3 +1,12 @@
 import os, platform, time, sys
-os.system('git pull')
-import KHAN_enc
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+    print('\033[0;97m[•] \033[1;32mCONGRATULATIONS 64BIT SUCCESS')
+    from Project1 import KHAN
+    
